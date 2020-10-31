a3c6d452c8591e8e7a0dc2f44761d268d5f1ce06
Aldrin <aldrinjunior@gmail.com>
2020-10-30 23:53:48 -0300

    lecture 1

---
+++ cs193/cs193/ContentView.swift
@@ -0,0 +1,65 @@
+//
+//  ContentView.swift
+//  cs193
+//
+//  Created by aldrin jr on 30/10/20.
+//  Copyright © 2020 aldrin jr. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+             ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+            .padding(10)
+            .foregroundColor(Color.orange)
+            .font(Font.largeTitle)
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+            RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
